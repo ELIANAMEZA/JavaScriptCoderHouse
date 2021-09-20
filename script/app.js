c24ff2d7bc2221d49c9c22336ce3125a9b33b6b6
ELIANAMEZA <89050440+ELIANAMEZA@users.noreply.github.com>
2021-09-20 12:30:38 -0300

    Add files via upload

---
+++ script/app.js
@@ -0,0 +1,8 @@
+//Desafío clase 1
+
+let nombre = prompt("ingrese su nombre");
+let apellido = prompt("ingrese su apellido");
+let lugar = prompt("donde vive");
+
+mensaje = `tu nombre completo es ${nombre} ${apellido} y vivís en ${lugar}`
+alert(mensaje)
