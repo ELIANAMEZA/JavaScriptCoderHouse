fce9c4bcfe522849040ef771b3dde771cf77cbbf
ELIANAMEZA <89050440+ELIANAMEZA@users.noreply.github.com>
2021-09-21 12:11:03 -0300

    Add files via upload

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
-//Desafío clase 1
+/* Desafío clase 1
 
 let nombre = prompt("ingrese su nombre");
 let apellido = prompt("ingrese su apellido");
@@ -6,3 +6,17 @@ let lugar = prompt("donde vive");
 
 mensaje = `tu nombre completo es ${nombre} ${apellido} y vivís en ${lugar}`
 alert(mensaje)
+
+*/
+
+// Desafío clase 2
+
+let edad = prompt("¿que edad tenés?")
+
+if (edad > 40) {
+    alert("podes hacerte un tatoo y tenés un 20%")
+} else if (edad >= 18) {
+    alert("podes hacerte el tatoo")
+} else {
+    alert("no podes hacerte el tatoo")
+}
