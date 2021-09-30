8c32c332ee255174d2a173c3dee5e8e57a14a5ef
ELIANAMEZA <89050440+ELIANAMEZA@users.noreply.github.com>
2021-09-29 23:10:49 -0300

    Add files via upload

---
+++ script/simulador.js
@@ -0,0 +1,26 @@
+//Desafío 4
+
+
+//Simulador de pago
+
+
+
+const suma = (a, b) => a + b;
+const resta = (a, b) => a - b;
+const calcular = (a, b) => a * b;
+const iva = x => x * 0.21;
+
+let precioProducto = 500;
+let precioDescuento = 50;
+let producto = prompt("que producto queres?");
+let cantidad = prompt("cuantos queres?");
+
+//Calculo el precioProducto + IVA 
+let nuevoPrecio = suma(precioProducto, iva(precioProducto))
+console.log(nuevoPrecio)
+    //Calculo el precioProducto + IVA  por cantidad
+let nuevoPrecioCantidad = calcular(suma(precioProducto, iva(precioProducto)), cantidad)
+console.log(nuevoPrecioCantidad)
+    //Calculo el precioProducto + IVA - precioDescueto
+let nuevoPrecioConDescuento = resta(suma(precioProducto, iva(precioProducto)), precioDescuento);
+console.log(nuevoPrecioConDescuento);
