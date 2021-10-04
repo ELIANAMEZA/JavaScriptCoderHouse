65d61f52a74700a20c7cb75406c419d7b511354f
ELIANAMEZA <89050440+ELIANAMEZA@users.noreply.github.com>
2021-10-04 10:56:45 -0300

    Add files via upload

--- script/simulador.js
+++ script/simulador.js
@@ -1,9 +1,5 @@
-//Desafío 4 y complementario
-
-
+/*//Desafío 4 y complementario
 //Simulador de pago
-
-
 
 const suma = (a, b) => a + b;
 const resta = (a, b) => a - b;
@@ -24,3 +20,36 @@ console.log(nuevoPrecioCantidad)
     //Calculo el precioProducto + IVA - precioDescueto
 let nuevoPrecioConDescuento = resta(suma(precioProducto, iva(precioProducto)), precioDescuento);
 console.log(nuevoPrecioConDescuento);
+*/
+
+//Desafío 5
+
+class Producto {
+    constructor(nombreProducto, categoriaProducto, precioProducto, stockProducto, disponibleProducto) {
+        this.nombre = nombreProducto
+        this.categoria = categoriaProducto
+        this.precio = precioProducto
+        this.stock = stockProducto
+        this.disponible = disponibleProducto
+    }
+    comprar(cantidad) {
+        if (this.stock <= 0) {
+            console.log("no podes comprar");
+            this.disponible = false
+        } else {
+            this.stock = this.stock - cantidad
+            console.log(`Compraste un ${this.nombre}, te salio ${this.precio * cantidad}`);
+        }
+    }
+}
+const producto1 = new Producto("Cartera", "Petra", 5000, 5, true)
+
+console.log(producto1);
+
+const producto2 = new Producto("Cartera", "Helena", 3000, 5, true)
+
+console.log(producto2);
+
+const producto3 = new Producto("Cartera", "Atenea", 4000, 5, true)
+
+console.log(producto3);
