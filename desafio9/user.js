f558c10ba29ec2e7e1b7b9778e3b9a3882d232bb
ELIANAMEZA <89050440+ELIANAMEZA@users.noreply.github.com>
2021-10-18 22:46:40 -0300

    Add files via upload

---
+++ desafio9/user.js
@@ -0,0 +1,78 @@
+class User {
+
+    constructor(nombre, pass) {
+        this.nombre = nombre;
+        this.pass = pass
+    }
+}
+
+
+class Producto {
+
+    constructor(nombre, categoria, precio, stock) {
+        this.nombre = nombre;
+        this.categoria = categoria;
+        this.precio = precio;
+        this.stock = stock;
+    }
+
+}
+
+
+const eliana = new User("meza", "peña")
+
+let listaProductos;
+
+if (localStorage.getItem("lista") == null) {
+    listaProductos = []
+} else {
+    listaProductos = JSON.parse(localStorage.getItem("lista"))
+}
+
+
+
+
+const crearProducto = (nombre, categoria, precio, stock) => {
+    const producto = new Producto(nombre, categoria, precio, stock)
+    return producto
+}
+
+
+const confirmacionDeAcceso = () => {
+
+    let bucle = true
+
+    while (bucle) {
+
+        const nombre = prompt("Ingresa tu nombre");
+        const pass = prompt("Ingresa tu pass")
+
+        if (nombre === eliana.nombre && pass === eliana.pass) {
+            bucle = false
+
+            let ciclo = true;
+
+            while (ciclo) {
+
+                const nombre = prompt("ingrese nombre del producto");
+                const categoria = prompt("ingrese categoria del producto");
+                const precio = Number(prompt("ingrese precio del producto"));
+                const stock = parseInt(prompt("Ingrese la cantidad de productos"))
+
+                listaProductos.push(crearProducto(nombre, categoria, precio, stock))
+
+                localStorage.setItem("lista", JSON.stringify(listaProductos))
+
+                ciclo = confirm("Deseas agregar otro mas?")
+            }
+
+            break;
+        } else {
+            alert("USUARIO INCORRECTO");
+        }
+
+    }
+
+}
+
+confirmacionDeAcceso()
